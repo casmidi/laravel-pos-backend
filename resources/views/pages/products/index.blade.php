41daf7af9bf9b800f731e260d9a56b8a085b4270
casmidi <casmidiasli@yahoo.co.id>
2023-12-22 17:51:41 +0700

    commit debug product to server

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -76,15 +76,15 @@
                                                 <td>{{ $product->name }} </td>
                                                 <td>{{ $product->category }} </td>
                                                 <td>{{ $product->price }} </td>
-                                                <td>
-                                                    {{ $product->image }}
+                                                <td>{{ $product->image }} </td>
+                                                    {{-- {{ $product->image }} --}}
                                                     {{-- @if ($product->image)
                                                         <img src="{{ asset('storage/products/' . $product->image) }}"
                                                             alt="" width="100px" class="img-thumbnail">
                                                     @else
                                                         <span class="badge badge-danger">No Image</span>
                                                     @endif --}}
-                                                </td>
+                                                {{-- </td> --}}
                                                 <td>
                                                     <div class="d-flex justify-content-center">
                                                         <a href="{{ route('product.edit', $product->id) }}">
