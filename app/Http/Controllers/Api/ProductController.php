73d9539e8d3a2c58507acc78017fae7243c4bff0
casmidi <casmidiasli@yahoo.co.id>
2023-12-24 22:08:11 +0700

    commit product controoller ada yang kelewat

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -1,55 +1,74 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(Request $request)
     {
-        //all products
-        $products= \App\Models\Product::orderBy('id','desc')->get();
-        return response()->json([
-            'success' => true,
-            'message' => 'List Data Product',
-            'data' => $products
-        ],200);
+        //get data products
+        $products = DB::table('products')
+            ->when($request->input('name'), function ($query, $name) {
+                return $query->where('name', 'like', '%' . $name . '%');
+            })
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+        //sort by created_at desc
+
+        return view('pages.products.index', compact('products'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    public function create()
+    {
+        return view('pages.products.create');
+    }
+
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|min:3|unique:products',
+            'price' => 'required|integer',
+            'stock' => 'required|integer',
+            'category' => 'required|in:food,drink,snack',
+            'image' => 'required|image|mimes:png,jpg,jpeg'
+        ]);
+
+        $filename = uniqid() . '.' . $request->image->extension();
+        $request->image->storeAs('public/products', $filename);
+        $data = $request->all();
+
+        $product = new \App\Models\Product;
+        $product->name = $request->name;
+        $product->price = (int) $request->price;
+        $product->stock = (int) $request->stock;
+        $product->category = $request->category;
+        $product->image = $filename;
+        $product->save();
+
+        return redirect()->route('product.index')->with('success', 'Product successfully created');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function edit($id)
     {
-        //
+        $product = \App\Models\Product::findOrFail($id);
+        return view('pages.products.edit', compact('product'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $data = $request->all();
+        $product = \App\Models\Product::findOrFail($id);
+        $product->update($data);
+        return redirect()->route('product.index')->with('success', 'Product successfully updated');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $product = \App\Models\Product::findOrFail($id);
+        $product->delete();
+        return redirect()->route('product.index')->with('success', 'Product successfully deleted');
     }
 }
