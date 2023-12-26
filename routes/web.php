f768b9c2017ede077bea416944fb3c31aa9d52e4
casmidi <casmidiasli@yahoo.co.id>
2023-12-26 16:18:28 +0700

    upload biar sukses di server

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +34,18 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('product',ProductController::class);
 
 });
+
+
+Route::get('storage/products/{filename}', function ($filename) {
+    $path = storage_path('app/public/products/' . $filename);
+
+    if (!Storage::exists('public/products/' . $filename)) {
+        abort(404);
+    }
+
+    return response()->file($path);
+})->where('filename', '.*');
+
 
 // Route::get('/login', function () {
 //     return view('pages.auth.login');
