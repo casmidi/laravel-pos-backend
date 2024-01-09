3cfe48607869c47e8cf1072c335756459ec1ec19
casmidi <casmidiasli@yahoo.co.id>
2024-01-09 14:59:33 +0700

    commit api product controller

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -11,7 +11,7 @@ class ProductController extends Controller
     {
         //get data products
         // $products = \App\Models\Product::orderBy('id','desc')->get();
-        $products= \App\Models\Product::orderBy('id','desc');
+        $products= \App\Models\Product::orderBy('id','desc')->get();
         return response()->json([
             'success' => true,
             'message' => 'List Data Product',
@@ -22,9 +22,36 @@ class ProductController extends Controller
    /**
     * store a newly created resource in storage.
     */
+    public function create()
+    {
+        return view('pages.products.create');
+    }
+
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => 'required|min:3|unique:products',
+            'price' => 'required|integer',
+            'stock' => 'required|integer',
+            'category' => 'required|in:food,drink,snack',
+            'image' => 'required|image|mimes:png,jpg,jpeg'
+        ]);
+
+        $filename = uniqid() . '.' . $request->image->extension();
+        $request->image->storeAs('public/products',$filename);
         $data = $request->all();
+
+        $product = new \App\Models\Product;
+        $product->name = $request->name;
+        $product->description = $request ->description;
+        $product->price = (int) $request->price;
+        $product->stock = (int) $request->stock;
+        $product->category = $request->categoty;
+        $product->image = $filename;
+        $product->save();
+        
+        // \App\Models\Product::create($data);
+        return redirect()->route('product.index')->with('success','Product successfully created');
     }
 
     /*
@@ -41,11 +68,17 @@ class ProductController extends Controller
 
     public function update(Request $request, string $id)
     {
-        //
+        $data = $request->all();
+        $product = \App\Models\Product::findOrFail($id);
+        $product->update($data);
+        return redirect()->route('product.index')->with('success','Product successfully update');
+
     }
 
     public function destroy($id)
     {
-        //
+        $product = \App\Models\Product::findOrFail($id);
+        $product->delete();
+        return redirect()->route('product.index')->with('success','Product successfully deleted');
     }
 }
